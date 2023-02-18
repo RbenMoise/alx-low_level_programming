4a4d3ea68b79314aada2f6f552c35b99f8ab3a45
RbenMoise <reubenmoses91@gmail>
2023-02-18 15:26:18 -0800

    numbez

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: (0) success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar (i);
+	putchar ('\n');
+	ruturn (0);
+}	
