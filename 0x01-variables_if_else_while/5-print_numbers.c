1a14164a67728839085d13f6ad383ed94f5942c5
RbenMoise <reubenmoses91@gmail>
2023-02-18 15:11:23 -0800

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - entry point
- * return 0 
+ * return 0 (success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = 0; i <=9; i++)
-		printf("%d \n", i);
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
 	return (0);
 }
