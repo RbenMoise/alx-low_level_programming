6f82ec5098456f6140b1aaa1337b7a6cc8741066
RbenMoise <reubenmoses91@gmail>
2023-02-19 01:16:02 -0800

    two by two

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (int i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			if (i != j)
+			{
+				int n1 = i / 10;
+				int n2 = i % 10;
+				int n3 = j / 10;
+				int n4 = j % 10;
+				putchar(n1 + '0');
+				putchar(n2 + '0');
+				putchar(' ');
+				putchar(n3 + '0');
+				putchar(n4 + '0');
+				if (i < 99)
+				{
+					putchar(',');
+					putchar(' ';)
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
