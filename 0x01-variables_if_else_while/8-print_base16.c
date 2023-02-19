390db6adb6f0011b4444f3f6e3e69a29171842cf
RbenMoise <reubenmoses91@gmail>
2023-02-18 23:15:16 -0800

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - entry point
- * return: 0 success
+ * return: 0 (success)
  */
 int main(void)
 {
@@ -19,5 +19,5 @@ int main(void)
 		  putchar(c);
 	  }
 	  putchar('\n');
-	  return 0;
+	  return (0);
 }
