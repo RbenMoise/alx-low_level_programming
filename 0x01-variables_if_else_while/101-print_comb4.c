9536dea0e5a3130c037f19c2530815d116dcaf0c
RbenMoise <reubenmoses91@gmail>
2023-02-19 01:01:27 -0800

    three combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,16 @@ int main(void)
 
 	for (i = 0; i <= 7; i++)
 	{
-		for (j = 2; j <= 8; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = 3; k <= 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				if (i != j && j != k && i != k)
 				{
 					putchar('0' + i);
 					putchar(j + '0');
 					putchar(k + '0');
-					if (i <7)
+					if (i < 7)
 					{
 						putchar(',');
 						putchar(' ');
