ac4073de33a67e473fd2650971622da3d14b87df
RbenMoise <reubenmoses91@gmail>
2023-02-18 23:37:20 -0800

    , and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *  Return (0) (success)
+ */
+
+int main
+{
+	int i;
+
+	for (i = 0; i < 10; i++);
+		putchar(i);
+
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		
+	putchar('\n');
+	return (0);
+}
+
