c165553a451ccd901d651a055faf52ed65d97243
RbenMoise <reubenmoses91@gmail>
2023-02-19 00:49:44 -0800

    three combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:0 success
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <=7; i++)
+	{
+		for (j = 2; j <= 8; j++)
+		{
+			for (k = 3; k <= 9; k++)
+			{
+				if (i != j && j != k && i != k)
+				{
+					putchar('0' +i);
+					putchar(j + '0');
+					putchar(k + '0');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0;)
+}
