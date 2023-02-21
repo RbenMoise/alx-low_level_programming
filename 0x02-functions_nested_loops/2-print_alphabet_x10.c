84dd7b59da662bb707fff7712c2d9cb526dc7452
RbenMoise <reubenmoses91@gmail>
2023-02-21 12:36:40 -0800

    10 alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void);
+{
+	int c = 0;
+	char l;
+
+	while (c <= 9)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+			_putchar(l);
+
+		_putchar('\n');
+	}
+}
