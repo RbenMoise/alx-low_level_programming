4421c453e395c9c89ab936dd9a8dc0a5c08c97fa
RbenMoise <reubenmoses91@gmail>
2023-02-22 11:28:44 -0800

    natural numbers from n

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_to_98 - prints n to 98 counts
+ * @n: input
+ */
+
+void print_to_98(int n)
+{
+	int c;
+
+	if (n > 98)
+		for (c = n; c >= 98; c--)
+			printf("%d, ", c);
+	else
+		for (c = n; c <= 98; n++)
+			printf("%d, " c);
+}
