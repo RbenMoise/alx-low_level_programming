9c1651b635394b24763f8df69ccddad24ef4335a
RbenMoise <reubenmoses91@gmail>
2023-02-22 06:00:52 -0800

    hours and minuets

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * void jack_bauer - entry point
+ * jack_bauer - entry point
  */
 
 void jack_bauer(void)
@@ -10,7 +10,7 @@ void jack_bauer(void)
 	for (h = 0; h <= 23; h++)
 	{
 		for (m = 0; m <= 59; m++)
-		{ 
+		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar(':');
