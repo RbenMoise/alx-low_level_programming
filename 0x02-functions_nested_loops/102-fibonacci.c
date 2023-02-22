c07e4244eb4f436f7a3a1c213bf3c7515a5c6a76
RbenMoise <reubenmoses91@gmail>
2023-02-22 12:45:44 -0800

    fibonachi

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int c;
+
+	unsigned long n1 = 0, n2 = 1, sum;
+
+	for (c = 0; c < 50; c++)
+	{
+		sum = n1 + n2;
+		printf("%lu", sum);
+
+		n1 = n2;
+		n2 = sum;
+
+		if (c == 49)
+			printf("\m");
+		else
+			printf(",")
+	}
+	return (0)
+}
