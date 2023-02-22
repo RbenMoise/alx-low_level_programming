aff5f2440655a4820704ba39afe11997f63d2231
RbenMoise <reubenmoses91@gmail>
2023-02-22 11:09:37 -0800

    adding two values

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * int add - adds two values
+ * add - adds two values
  * @num1: first value
  * @num2: second value
  * Return: x + y
  */
- 
+
 int add(int num1, int num2)
 {
 	return (num1 + num2);
