5731107a39794d9addad1050249b31a6ff83cb78
RbenMoise <reubenmoses91@gmail>
2023-02-23 13:18:50 -0800

    o

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _isupper - checks for the lower or uppercase
- * Returns: 1 if upper
- * else 0
+ * Returns: 1 if upper elsse 0
+ * @c: int to be checked
  */
 
 int _isupper(int c)
