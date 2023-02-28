225291b85eebce623b99581574e6d4ecb68af6e3
RbenMoise <reubenmoses91@gmail>
2023-02-28 03:51:56 -0800

    header

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
 int _putchar(char c);
 int _isdigit(int c);
@@ -10,7 +10,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-void print_square(int size);i
+void print_square(int size);
 void print_triangle(int size);
 
-#define
+#endif
