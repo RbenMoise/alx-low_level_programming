71950698376eb33826f557a3b64f9f4ed51e62e0
RbenMoise <reubenmoses91@gmail>
2023-03-02 10:54:37 -0800

    more

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -23,8 +23,9 @@ void print_line(char *c, int s, int l)
 		if (j % 2)
 			putchar(' ');
 	}
-		
-	for (k = 0; k <= s; k++){
+
+	for (k = 0; k <= s; k++)
+	{
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[l * 10 + k]);
 		else
