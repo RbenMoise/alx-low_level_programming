6e41d697725594cd273bffe516d4fa7d0f59c56f
RbenMoise <reubenmoses91@gmail>
2023-03-06 12:25:37 -0800

    all

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,7 @@
  * * @src: bytes from memory area
  * * @dest: to memory area
  * * Return: a pointer to @dest
- * */
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
