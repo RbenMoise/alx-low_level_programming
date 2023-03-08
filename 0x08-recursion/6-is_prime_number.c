1107ecae03b4c1ad5224c51909a7f08e66d373be
RbenMoise <reubenmoses91@gmail>
2023-03-08 12:27:04 -0800

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,5 @@
 #include "main.h"
 /**
- * is_prime_number - prime numbers
- * @n: value
- * Return: int
- */
-int is_prime_number(int n)
-{
-	if (n < 2)
-		return (0);
-	return check(n, 2);
-}
-/**
  * check - recursive
  * @i: current devisor
  * @n: to be checked
@@ -21,8 +10,19 @@ int check(int a, int b)
 {
 	if (i == n)
 		return (1);
-	if (n % i == 0)
+	if (n % a == 0)
 		return (0);
-	return (check(n, i + 1));
+	return (check(n, a + 1));
+}
+/**
+ * is_prime_number - prime numbers
+ * @n: value
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return check(n, 2);
 }
 
