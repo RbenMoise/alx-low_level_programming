7f3eb870bab2c1d6492cc6e002dbdc491942cdfb
RbenMoise <reubenmoses91@gmail>
2023-03-08 06:06:33 -0800

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints in revers
+ * @s: string
+ * Return:
+ */
+
+void _print_rev_recursion(char *s);
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
