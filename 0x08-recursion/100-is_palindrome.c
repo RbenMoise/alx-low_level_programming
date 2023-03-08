23e67352682927a602b2df04f9fc4e01cabe8cdd
RbenMoise <reubenmoses91@gmail>
2023-03-08 12:54:40 -0800

    phalindrom

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,27 @@
+#include <ctype.h>
+i#include <string.h>
+
+/**
+ *  * is_palindrome - checks if a string is a palindrome
+ *   * @s: string to check
+ *    * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+	int i, j;
+
+	for (i = 0, j = len - 1; i < j; i++, j--) {
+		while (!isalnum(s[i]) && i < j) {
+			i++;
+		}
+		while (!isalnum(s[j]) && i < j) {
+			j--;
+		}
+		if (tolower(s[i]) != tolower(s[j])) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
