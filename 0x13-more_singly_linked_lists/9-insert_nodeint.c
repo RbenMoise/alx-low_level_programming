3a460f240ee1ef7fe44da20aa1e213bb8c7ebab6
RbenMoise <reubenmoses91@gmail>
2023-04-04 15:32:16 -0700

    tierd of 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,32 +11,30 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int j;
-	listint_t *temp, *nnew;
+	listint_t *new_node, *temp;
+	unsigned int j = 0;
 
-	if (head == NULL)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 	if (idx != 0)
 	{
-		temp = *head;
-		for (j = 0; j < idx - 1 && temp != NULL; j++)
-		{
-			temp = temp->next;
-		}
-		if (temp == NULL)
-			return (NULL);
-	}
-	nnew = malloc(sizeof(listint_t));
-	if (nnew == NULL)
+	temp = *head;
+	for (; j < idx - 1 && temp != NULL; j++)
+		temp = temp->next;
+	if (temp == NULL)
 		return (NULL);
-	nnew->n = n;
+	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
 	if (idx == 0)
 	{
-		nnew->next = *head;
-		*head = nnew;
-		return (nnew);
-	} 
-	nnew->next = temp->next;
-	temp->next = nnew;
-	return (nnew);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
