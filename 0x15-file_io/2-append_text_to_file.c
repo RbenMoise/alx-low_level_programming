1bb401ef1de5a391d63f93d9b1f734171ce11706
RbenMoise <reubenmoses91@gmail>
2023-04-10 13:51:09 -0700

    tough

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,32 @@
-#include "main.h"  
+#include "main.h"
+
 /**
- * create_file -thhis creates the files
- * @text_content: this is the content to write in the file
- * @filename: this is the file name to be created
- * Return: this will return 1 when it is success, returns -1 when it  failures
+ * append_text_to_file - this will apend the text to the files
+ * @filename: file 
+ * @text_content: content fir the file to be appended
+ * Return: 1 if it is success and -1 if it is not successfull
  */
-int create_file(const char *filename, char *text_content)
+
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int opn, wrt, len = 0;
+	int opn, len, wrt;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wrt = write(opn, text_content, len);
-
-	if (opn == -1 || wrt == -1)
+	opn = open(filename, O_APPEND | O_WRONLY);
+	if (opn == -1)
 		return (-1);
-
-	close(opn);
-
+	if (text_content == NULL)
+	{
+		close(opn);
+		return (1);
+	}
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+	wrt = write(opn, text_content, len);
+	if (wrt < 1)
+		return (-1);
+	close(fd);
 	return (1);
 }
