110b8b4fe78d1313d1e0f0b5cc69bd91d28b2877
RbenMoise <reubenmoses91@gmail>
2023-04-11 05:24:10 -0700

    most difficult

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,38 @@
 #include "main.h"
 
 /**
- * _strlen - this function will help find the length of the string
- * @str: this is the string pointer
- * Return: this will give us the len of the string
+ * _strlen - this function will help us to find the lenth of the string
+ * @str: string pointer
+ * Return: len
  */
 size_t _strlen(char *str)
 {
-	size_t k;
+	size_t me;
 
-	for (k = 0; str[k]; k++)
+	for (me = 0; str[me]; me++)
 		;
-	return (k);
+	return (me);
 }
 
 /**
- * append_text_to_file - appends text
- * @filename:  file name
- * @text_content: content
- * Return: 1 if it is  successfull and it will return  -1 if it failures
+ * append_text_to_file - append to the end of the file
+ * @filename: file name
+ * @text_content:content of the file
+ * Return: 1 otherwise 0
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	/**p is the lenght to be checked*/
-	int j;
-	ssize_t p;
+	int po;
+	ssize_t sz;
 
 	if (filename == NULL)
 		return (-1);
-	j = open(filename, O_WRONLY | O_APPEND);
-	if (j == -1)
+	po = open(filename, O_WRONLY | O_APPEND);
+	if (po == -1)
 		return (-1);
 	if (text_content != NULL)
-		p = write(j, text_content, _strlen(text_content));
-	close(j);
-	if (len == -1)
+		sz = write(po, text_content, _strlen(text_content));
+	close(po);
+	if (sz == -1)
 		return (-1);
-	return (1);
 }
