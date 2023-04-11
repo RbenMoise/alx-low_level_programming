6ec4e320f59e5afcc84891c095ac0d9ed3fcdb71
RbenMoise <reubenmoses91@gmail>
2023-04-11 04:38:47 -0700

    just trying my luck

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,72 +1,110 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * error_file - this function will checks if files can be openedor not
- * @file_from: for filling the form file
- * @file_to: file
- * @argv: x
- * Return: 0.
+ * check1 - no of arg
+ * @argc: c
+ *
+ * Return: void
  */
-void error_file(int file_from, int file_to, char *argv[])
+void check1(int argc)
 {
-	if (file_from == -1)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Cant write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(1);
 	}
 }
 
 /**
- * main - entry poin
- * @argc:c
- * @argv: v
+ * check2 - check if exist 
+ * @check: this will checks if true of false
+ * @file: file name
+ * @fd_from: filedesc
+ * @fd_to: filedesc
+ * Return:  nothisnd
+ */
+void check2(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(2);
+	}
+}
+
+/**
+ * check3 - checks for the created files
+ * @check: this will chec if true of false
+ * @file: file_to name
+ * @fd_from: file desc
+ * @fd_to: file dsc
+ * Return: nothing
+ */
+void check3(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(3);
+	}
+}
+
+/**
+ * check4 - checks of file was closed with no errors
+ * @check: this willchecks if true or false
+ * @fd: file dsc
+ * Return:nothing
+ */
+void check4(int check, int fd)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(4);
+	}
+}
+/**
+ * main - this transfers the content of the file to the other file
+ * @argc: this rep the no of arg passed 
+ * @argv: pointer array to the arguments
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, errorclose;
-	ssize_t nc, w;
-	char buffer[1024];
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t len1, len2;
+	char buf[1024];
+	mode_t file_r;
 
-	if (argc != 3)
+	check1(argc);
+	fd_from = open(argv[1], O_RDONLY);
+	check2((ssize_t)fd_from, argv[1], -1, -1);
+	file_r = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_r);
+	check3((ssize_t)fd_to, argv[2], fd_from, -1);
+	len1 = 1024;
+	while (len1 == 1024)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
+		len1 = read(fd_from, buf, 1024);
+		check2(len1, argv[1], fd_from, fd_to);
+		len2 = write(fd_to, buf, len1);
+		if (len2 != len1)
+			len2 = -1;
+		check3(len2, argv[2], fd_from, fd_to);
 	}
-
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
-
-	nc = 1024;
-	while (nc == 1024)
-	{
-		nc = read(file_from, buffer, 1024);
-		if (nc == -1)
-			error_file(-1, 0, argv);
-		w = write(file_to, buffer, nc);
-		if (w == -1)
-			error_file(0, -1, argv);
-	}
-
-	errorclose = close(file_from);
-	if (errorclose == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-
-	errorclose = close(file_to);
-	if (errorclose == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check4(close_to, fd_to);
+	check4(close_from, fd_from);
 	return (0);
-}
+} 
