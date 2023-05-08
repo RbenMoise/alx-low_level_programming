46b0b34f5242afba3d3b47e4968a5ef3d9734c8a
RbenMoise <reubenmoses91@gmail.com>
2023-05-08 08:36:37 -0700

    the first task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * create_file -thhis creates the files
+ * reate_file -thhis creates the files
  * @text_content: this is the content to write in the file
  * @filename: this is the file name to be created
  * Return: this will return 1 when it is success, returns -1 when it  failures
  */
 int create_file(const char *filename, char *text_content)
 {
-	int opn, wrt, len = 0;
+	int opn, wrt, len = 0 ; 
 
 	if (filename == NULL)
 		return (-1);
